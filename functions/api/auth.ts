f895e8a5e4f7b439e4dd6a86934c925bf0404a66
Paul Queruel <Poooel@users.noreply.github.com>
2022-09-02 00:43:56 +0100

    Add Reconnect and History (#11)
    * Add reconnect and history
    
    * update channel name

--- functions/api/auth.ts
+++ functions/api/auth.ts
@@ -4,12 +4,15 @@ import Ably from 'ably/build/ably-webworker.min'
 import { Env } from '../utils/env'
 
 export const onRequestGet: PagesFunction<Env> = async (context) => {
-  const { env } = context
+  const { env, request } = context
+
+  const { searchParams } = new URL(request.url)
+  const clientId = searchParams.get('clientId')
 
   const client = new Ably.Rest.Promise(env.ABLY_CLIENT_SIDE_API_KEY)
 
   const tokenRequestData = await client.auth.createTokenRequest({
-    clientId: randomName()
+    clientId: clientId ?? randomName()
   })
 
   return jsonResponse(tokenRequestData, { status: 200 })
