d99eb6960a610c247ec61533879a9daadc563368
Paul Queruel <Poooel@users.noreply.github.com>
2022-10-01 00:34:06 +0100

    Add debug info to Sentry events (#33)

--- functions/types/env.ts
+++ functions/types/env.ts
@@ -1,6 +1,7 @@
 export interface Env {
   ABLY_SERVER_SIDE_API_KEY: string
   ABLY_CLIENT_SIDE_API_KEY: string
+  SENTRY_DSN: string
   GAME_STATE_STORE: KVNamespace
   ABLY_JWT_STORE: KVNamespace
 }
