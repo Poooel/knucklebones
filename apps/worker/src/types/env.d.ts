21ef9733b31923be71b3ccce6bd946c09c9c2d87
Paul Queruel <paul.queruel@protonmail.com>
2022-10-04 00:50:22 +0100

    Fix env.d.ts

--- apps/worker/src/types/env.d.ts
+++ apps/worker/src/types/env.d.ts
@@ -2,5 +2,5 @@ export interface Env {
   ABLY_SERVER_SIDE_API_KEY: string
   ABLY_CLIENT_SIDE_API_KEY: string
   ABLY_JWT_STORE: KVNamespace
-  GAME_STATE: DurableObjectNamespace
+  GAME_STATE_STORE: DurableObjectNamespace
 }
