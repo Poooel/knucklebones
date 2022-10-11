d37a0bd0f31df90dcf208ebf0fe6768dcf069fa6
Alexis LUCAS <sixela.sacul.pro@protonmail.com>
2022-10-11 18:55:24 +0100

    Reverted layout (#41)
    * Reverted layout
    
    * fixed icon
    
    * Refactored layout
    
    - Used custom grid layout
    - Handled longer names, but limited length
    - Allowed classes on IconButton
    - Used IconButton for mobile logs

--- apps/front/src/components/IconButton.tsx
+++ apps/front/src/components/IconButton.tsx
@@ -1,17 +1,22 @@
 import * as React from 'react'
+import clsx from 'clsx'
 
 interface IconButtonProps {
   icon: JSX.Element
+  className?: string
   onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 
-export function IconButton({ icon, onClick }: IconButtonProps) {
+export function IconButton({ icon, className, onClick }: IconButtonProps) {
   return (
     <button
-      className='text-slate-900 transition-colors duration-100 hover:text-slate-900/80 dark:text-slate-200 dark:hover:text-slate-50/80'
+      className={clsx(
+        'text-slate-900 transition-colors duration-100 hover:text-slate-900/80 dark:text-slate-200 dark:hover:text-slate-50/80',
+        className
+      )}
       onClick={onClick}
     >
-      <i className='aspect-square h-6'>{icon}</i>
+      <div className='aspect-square h-6'>{icon}</div>
     </button>
   )
 }
