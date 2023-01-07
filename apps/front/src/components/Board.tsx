cfd87497b299931921fdeee0ec42d7a69f3c732d
Alexis LUCAS <sixela.sacul.pro@protonmail.com>
2023-01-07 17:47:04 +0100

    Brought column score from how-to-play
    - Applied opacity when not playing only to the dice board
    - Fixed font weight on the column score

--- apps/front/src/components/Board.tsx
+++ apps/front/src/components/Board.tsx
@@ -5,7 +5,7 @@ import { Dice } from './Dice'
 import { Column } from './Column'
 import { Cell } from './Cell'
 import { Name } from './Name'
-import { ColumnScore } from './ColumnScore'
+import { ColumnScoreTooltip  } from './ColumnScore'
 
 interface BoardProps extends IPlayer {
   isPlayerOne: boolean
@@ -43,7 +43,6 @@ export function Board({
       className={clsx('flex items-center gap-1 md:gap-4', {
         'flex-col': isPlayerOne,
         'flex-col-reverse': !isPlayerOne,
-        'opacity-75': !canPlay,
         'font-semibold': canPlay
       })}
     >
@@ -71,7 +70,7 @@ export function Board({
         >
           <div className='grid w-full grid-cols-3'>
             {scorePerColumn.map((score, index) => (
-              <ColumnScore
+              <ColumnScoreTooltip 
                 score={score}
                 dice={columns[index]}
                 isPlayerOne={isPlayerOne}
@@ -79,7 +78,9 @@ export function Board({
               />
             ))}
           </div>
-          <div className='grid aspect-square grid-cols-3 divide-x-2 divide-slate-300 rounded-lg bg-transparent shadow-lg shadow-slate-300 dark:divide-slate-800 dark:shadow-slate-800'>
+          <div className={clsx('grid aspect-square grid-cols-3 divide-x-2 divide-slate-300 rounded-lg bg-transparent shadow-lg shadow-slate-300 dark:divide-slate-800 dark:shadow-slate-800', {
+            'opacity-75': !canPlay
+          })}>
             {COLUMNS_PLACEHOLDER.map((_, colIndex) => {
               const column = columns[colIndex]
               const countedDice = countDiceInColumn(column)
--- apps/front/src/components/ColumnScore.tsx
+++ apps/front/src/components/ColumnScore.tsx
@@ -19,32 +19,61 @@ function CountedDice({ value, count }: CountedDiceProps) {
     return <Dice value={value} count={count} variant='small' />
   }
   return (
+    // TODO: div (dice) shouldn't be in p
     <p className='flex flex-row items-center gap-1'>
-      <span>((</span>
+      <span>(</span>
       {Array.from({ length: count }).map((_, i) => (
-        <>
-          <Dice value={value} count={count} variant='small' key={i} />
+        <React.Fragment key={i}>
+          <Dice value={value} count={count} variant='small' />
           {i + 1 < count && <span>+</span>}
-        </>
+        </React.Fragment>
       ))}
-      <span>) x {count})</span>
+      <span>) x {count}</span>
     </p>
   )
 }
 
 interface ColumnScoreProps {
-  isPlayerOne: boolean
   dice: number[]
   score: number
+  showScore?: boolean
 }
 
-export function ColumnScore({ dice, score, isPlayerOne }: ColumnScoreProps) {
+export function ColumnScore({
+  dice,
+  score,
+  showScore = false
+}: ColumnScoreProps) {
+  const countedDice = countDiceInColumn(dice)
+
+  return (
+    <div className='flex flex-row items-center gap-2 font-normal'>
+      {[...countedDice.entries()].map(([value, count], index) => (
+        <React.Fragment key={index}>
+          <CountedDice value={value} count={count} />
+          {index + 1 < countedDice.size && <span>+</span>}
+        </React.Fragment>
+      ))}
+      {showScore && <span> = {score}</span>}
+    </div>
+  )
+}
+
+interface ColumnScoreTooltipProps extends ColumnScoreProps {
+  isPlayerOne: boolean
+}
+
+export function ColumnScoreTooltip({
+  dice,
+  score,
+  isPlayerOne
+}: ColumnScoreTooltipProps) {
   const [showTooltip, setShowTooltip] = React.useState(false)
   const { x, y, reference, floating, strategy, context } = useFloating({
     placement: isPlayerOne ? 'top' : 'bottom',
     open: showTooltip,
     onOpenChange(open) {
-      if (score > 0) {
+      if (score > 0 || !open) {
         setShowTooltip(open)
       }
     }
@@ -55,14 +84,12 @@ export function ColumnScore({ dice, score, isPlayerOne }: ColumnScoreProps) {
     useDismiss(context)
   ])
 
-  const countedDice = countDiceInColumn(dice)
-
   return (
     <>
       <p ref={reference} className='text-center' {...getReferenceProps()}>
         {score}
       </p>
-      {showTooltip && (
+      {showTooltip && score > 0 && (
         <div
           ref={floating}
           style={{
@@ -73,14 +100,7 @@ export function ColumnScore({ dice, score, isPlayerOne }: ColumnScoreProps) {
           className='w-max rounded bg-slate-200 p-2 text-slate-900 shadow shadow-slate-300 dark:bg-slate-700 dark:text-slate-50 dark:shadow-slate-800'
           {...getFloatingProps()}
         >
-          <div className='flex flex-row items-center gap-2'>
-            {[...countedDice.entries()].map(([value, count], index) => (
-              <React.Fragment key={index}>
-                <CountedDice value={value} count={count} />
-                {index + 1 < countedDice.size && <span>+</span>}
-              </React.Fragment>
-            ))}
-          </div>
+          <ColumnScore dice={dice} score={score} />
         </div>
       )}
     </>
