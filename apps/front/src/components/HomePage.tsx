58afff5c8e868afb5fdc13d30f67001f5798cbda
Paul Queruel <paul.queruel@protonmail.com>
2023-01-11 15:20:29 +0000

    First try at footer and legal notice

--- apps/front/src/components/HomePage.tsx
+++ apps/front/src/components/HomePage.tsx
@@ -5,6 +5,9 @@ import { useNavigate } from 'react-router-dom'
 import { v4 as uuidv4 } from 'uuid'
 import KnucklebonesLogo from '../svgs/logo.svg'
 import { Button } from './Button'
+import { Disclaimer } from './Disclaimer'
+import { CodeBracketIcon, EnvelopeIcon } from '@heroicons/react/24/outline'
+import { IconButton } from './IconButton'
 
 export function HomePage() {
   const navigate = useNavigate()
@@ -68,6 +71,31 @@ export function HomePage() {
           })}
         </div>
       </Transition>
+      <div className='absolute bottom-0 flex flex-col gap-2 p-2'>
+        <Disclaimer />
+        <div className='flex flex-row justify-center gap-4'>
+          <a
+            className='text-slate-900 transition-all hover:text-slate-900/80 dark:text-slate-200 dark:hover:text-slate-50/80'
+            href='https://github.com/Poooel/knucklebones'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <div className='aspect-square h-6'>
+              <CodeBracketIcon />
+            </div>
+          </a>
+          <a
+            className='text-slate-900 transition-all hover:text-slate-900/80 dark:text-slate-200 dark:hover:text-slate-50/80'
+            href='mailto:contact@knucklebones.io'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <div className='aspect-square h-6'>
+              <EnvelopeIcon />
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
