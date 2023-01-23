8f6bb17fa4956aca38f5a1018727540bc91dedf3
Paul Queruel <paul.queruel@protonmail.com>
2023-01-23 01:14:50 +0000

    Fix incorrect div

--- apps/front/src/components/HomePage.tsx
+++ apps/front/src/components/HomePage.tsx
@@ -7,7 +7,6 @@ import KnucklebonesLogo from '../svgs/logo.svg'
 import { Button } from './Button'
 import { Disclaimer } from './Disclaimer'
 import { CodeBracketIcon, EnvelopeIcon } from '@heroicons/react/24/outline'
-import { IconButton } from './IconButton'
 
 export function HomePage() {
   const navigate = useNavigate()
@@ -68,39 +67,39 @@ export function HomePage() {
               </Button>
             )
           })}
-        </div>
-      </Transition>
-      <Button
-        as={Link}
-        variant='large'
-        className='tracking-tight'
-        to='/how-to-play'
-      >
-        How to play
-      </Button>
-      <div className='absolute bottom-0 flex flex-col gap-2 p-2'>
-        <Disclaimer />
-        <div className='flex flex-row justify-center gap-4'>
-          <a
-            className='text-slate-900 transition-all hover:text-slate-900/80 dark:text-slate-200 dark:hover:text-slate-50/80'
-            href='https://github.com/Poooel/knucklebones'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <div className='aspect-square h-6'>
-              <CodeBracketIcon />
-            </div>
-          </a>
-          <a
-            className='text-slate-900 transition-all hover:text-slate-900/80 dark:text-slate-200 dark:hover:text-slate-50/80'
-            href='mailto:contact@knucklebones.io'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <div className='aspect-square h-6'>
-              <EnvelopeIcon />
-            </div>
-          </a>
+        </Transition>
+        <Button
+          as={Link}
+          variant='large'
+          className='tracking-tight'
+          to='/how-to-play'
+        >
+          How to play
+        </Button>
+        <div className='absolute bottom-0 flex flex-col gap-2 p-2'>
+          <Disclaimer />
+          <div className='flex flex-row justify-center gap-4'>
+            <a
+              className='text-slate-900 transition-all hover:text-slate-900/80 dark:text-slate-200 dark:hover:text-slate-50/80'
+              href='https://github.com/Poooel/knucklebones'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <div className='aspect-square h-6'>
+                <CodeBracketIcon />
+              </div>
+            </a>
+            <a
+              className='text-slate-900 transition-all hover:text-slate-900/80 dark:text-slate-200 dark:hover:text-slate-50/80'
+              href='mailto:contact@knucklebones.io'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <div className='aspect-square h-6'>
+                <EnvelopeIcon />
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </div>
