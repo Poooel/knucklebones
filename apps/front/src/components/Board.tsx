f96de498a6a3c854bed764877730a9af7ed84553
Alexis LUCAS <sixela.sacul.pro@protonmail.com>
2023-01-23 01:06:04 +0000

    Added how to play section (#55)
    * Added tooltip helper on column score
    
    - Added floating ui for handling tooltips
    - Added tooltip on score
    - Used countDice to explain scoring
    - Added mini variant to dice for smaller, inline versions
    
    * Addressed comments
    
    - Renamed mini variant into small
    - Removed unnecessary iinterfaces
    - Improved score formula
    
    * Brought column score from how-to-play
    
    - Applied opacity when not playing only to the dice board
    - Fixed font weight on the column score
    
    * Added how to play section
    
    - Added Modal component based on usage
    - Renamed Board into PlayerBoard
    - Added Board component, only showing the actual board
    - Used new simple board to show examples in the HowToPlay modal
    - Added HowToPlay in the toolbar
    
    * Small update
    
    * Re-used formula help from tooltip in how to play
    
    * temp
    
    * Consistency
    
    Co-authored-by: Paul Queruel <Poooel@users.noreply.github.com>

--- apps/front/src/components/Board.tsx
+++ apps/front/src/components/Board.tsx
@@ -5,12 +5,18 @@ import { Dice } from './Dice'
 import { Column } from './Column'
 import { Cell } from './Cell'
 import { Name } from './Name'
-import { ColumnScoreTooltip  } from './ColumnScore'
+import { ColumnScoreTooltip } from './ColumnScore'
 
-interface BoardProps extends IPlayer {
+interface BoardProps {
+  columns: IPlayer['columns']
+  // TODO: `isReversed` instead?
   isPlayerOne: boolean
   canPlay: boolean
+  diceClassName?: string
   onColumnClick?(colIndex: number): void
+}
+
+interface PlayerBoardProps extends IPlayer, BoardProps {
   updateDisplayName?(displayName: string): void
   isDisplayNameEditable?: boolean
   outcome: Outcome
@@ -25,6 +31,57 @@ const CELLS_PER_COLUMN_PLACEHOLDER = Array.from({
 })
 
 export function Board({
+  columns,
+  canPlay,
+  isPlayerOne,
+  diceClassName,
+  onColumnClick
+}: BoardProps) {
+  return (
+    <div
+      className={clsx(
+        'grid aspect-square grid-cols-3 divide-x-2 divide-slate-300 rounded-lg bg-transparent shadow-lg shadow-slate-300 dark:divide-slate-800 dark:shadow-slate-800',
+        {
+          'opacity-75': !canPlay
+        }
+      )}
+    >
+      {COLUMNS_PLACEHOLDER.map((_, colIndex) => {
+        const column = columns[colIndex]
+        const countedDice = countDiceInColumn(column)
+        const canPlayInColumn = canPlay && column.length < MAX_CELLS_PER_COLUMNS
+        return (
+          <Column
+            key={colIndex}
+            readonly={!canPlayInColumn}
+            onClick={
+              canPlayInColumn ? () => onColumnClick?.(colIndex) : undefined
+            }
+          >
+            {CELLS_PER_COLUMN_PLACEHOLDER.map((_, cellIndex) => {
+              // Reverses the render order to mirror the board for the other player
+              const actualCellIndex = !isPlayerOne
+                ? MAX_CELLS_PER_COLUMNS - cellIndex - 1
+                : cellIndex
+              const value = column[actualCellIndex]
+              return (
+                <Cell key={cellIndex}>
+                  <Dice
+                    value={value}
+                    count={countedDice.get(value)}
+                    className={diceClassName}
+                  />
+                </Cell>
+              )
+            })}
+          </Column>
+        )
+      })}
+    </div>
+  )
+}
+
+export function PlayerBoard({
   id: playerId,
   dice,
   isPlayerOne,
@@ -37,7 +94,7 @@ export function Board({
   updateDisplayName,
   isDisplayNameEditable = false,
   outcome
-}: BoardProps) {
+}: PlayerBoardProps) {
   return (
     <div
       className={clsx('flex items-center gap-1 md:gap-4', {
@@ -70,7 +127,7 @@ export function Board({
         >
           <div className='grid w-full grid-cols-3'>
             {scorePerColumn.map((score, index) => (
-              <ColumnScoreTooltip 
+              <ColumnScoreTooltip
                 score={score}
                 dice={columns[index]}
                 isPlayerOne={isPlayerOne}
@@ -78,40 +135,12 @@ export function Board({
               />
             ))}
           </div>
-          <div className={clsx('grid aspect-square grid-cols-3 divide-x-2 divide-slate-300 rounded-lg bg-transparent shadow-lg shadow-slate-300 dark:divide-slate-800 dark:shadow-slate-800', {
-            'opacity-75': !canPlay
-          })}>
-            {COLUMNS_PLACEHOLDER.map((_, colIndex) => {
-              const column = columns[colIndex]
-              const countedDice = countDiceInColumn(column)
-              const canPlayInColumn =
-                isPlayerOne && canPlay && column.length < MAX_CELLS_PER_COLUMNS
-              return (
-                <Column
-                  key={colIndex}
-                  readonly={!canPlayInColumn}
-                  onClick={
-                    canPlayInColumn
-                      ? () => onColumnClick?.(colIndex)
-                      : undefined
-                  }
-                >
-                  {CELLS_PER_COLUMN_PLACEHOLDER.map((_, cellIndex) => {
-                    // Reverses the render order to mirror the board for the other player
-                    const actualCellIndex = !isPlayerOne
-                      ? MAX_CELLS_PER_COLUMNS - cellIndex - 1
-                      : cellIndex
-                    const value = column[actualCellIndex]
-                    return (
-                      <Cell key={cellIndex}>
-                        <Dice value={value} count={countedDice.get(value)} />
-                      </Cell>
-                    )
-                  })}
-                </Column>
-              )
-            })}
-          </div>
+          <Board
+            isPlayerOne={isPlayerOne}
+            canPlay={isPlayerOne && canPlay}
+            columns={columns}
+            onColumnClick={onColumnClick}
+          />
         </div>
         <div className='my-4'>
           <p>Total: {score}</p>
