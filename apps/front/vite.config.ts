4fddd22522778020f7482f9e86056d70abd50172
Alexis LUCAS <sixela.sacul.pro@protonmail.com>
2024-01-14 12:24:30 +0100

    Revert "Fixed base URL"
    This reverts commit 78e4f4022081b030bb0f1fc9f7736e18c02af8d9.

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -5,18 +5,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => ({
+export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
-        // https://vitejs.dev/guide/build.html#multi-page-app
         default: resolve(__dirname, 'index.html'),
         en: resolve(__dirname, 'en/index.html'),
         fr: resolve(__dirname, 'fr/index.html')
       }
     }
   },
-  // https://vitejs.dev/config/shared-options.html#base
-  base: mode === 'production' ? 'https://knucklebones.io/' : '/'
-}))
+  base: process.env.CF_PAGES_URL ?? ''
+})
