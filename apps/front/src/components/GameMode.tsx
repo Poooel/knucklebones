54518feb736aa1d0c54955e75dbf6afa87d4fd21
Alexis LUCAS <sixela.sacul.pro@protonmail.com>
2024-04-03 00:19:34 +0200

    Added icon dice for game mode

--- apps/front/src/components/GameMode.tsx
+++ apps/front/src/components/GameMode.tsx
@@ -1,19 +1,30 @@
 import { useTranslation } from 'react-i18next'
+import { Button } from './Button'
+import { Dice } from './Dice'
 import { useGame } from './GameContext'
-import { InfoPopover } from './Popover'
-import { Text } from './Text'
+import { Popover, PopoverContent, PopoverTrigger } from './Popover'
 
 export function GameMode() {
   const { gameMode } = useGame()
   const { t } = useTranslation()
 
   return (
-    <div className='flex gap-2'>
-      <Text className='font-medium'>
-        {t('game-settings.game-mode.label')} :{' '}
-        {t(`game-settings.game-mode.${gameMode}`)}
-      </Text>
-      <InfoPopover>{t(`game-settings.game-mode.info`)}</InfoPopover>
-    </div>
+    <Popover>
+      <PopoverTrigger>
+        {/* Va falloir adapter Button et IconButton pour passer des refs
+        Malheureusement c'est pas super compatible avec les composants
+        polymorphiques, mais en même temps, on en a pas tellement besoin ici */}
+        <Button
+          variant='ghost'
+          leftIcon={<Dice value={5} variant='outline' size='small' />}
+        >
+          {t('game-settings.game-mode.label')} :{' '}
+          {t(`game-settings.game-mode.${gameMode}`)}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className='whitespace-pre-line'>
+        {t(`game-settings.game-mode.info`)}
+      </PopoverContent>
+    </Popover>
   )
 }
